79bf04c404b614373709a0470eac40cbba01219b
gautamV19 <ee20b144@smail.iitm.ac.in>
2021-12-03 22:11:19 +0530

    about cfi update

--- src/Components/Home/CfiVideo.tsx
+++ src/Components/Home/CfiVideo.tsx
@@ -15,7 +15,10 @@ function CfiVideo() {
       mt={3}
       alignItems="center"
     >
-      <Heading white="cfi " red="shots" />
+      <Heading white="about " red="cfi" />
+      <Typography variant="h4" color="primary">
+        ‘‘Walk in with an idea, walk out with a product.’’
+      </Typography>
       <Grid
         item
         container
@@ -26,26 +29,6 @@ function CfiVideo() {
         spacing={4}
       >
         <Grid item>
-          <Typography variant="body1" color="primary">
-            The Centre for Innovation (CFI) was established on the premise that
-            innovation emerges through community interaction and the freedom to
-            learn and experiment. Constituted in 2008, with the help of funds
-            raised by the class of 1981, this unique 24/7 student lab is located
-            at the New Academic Complex in the heart of campus. After almost a
-            decade of the team's zeal and passion, combined with a comprehensive
-            inventory, ample workspace, guidance from faculty and students, CFI
-            is a house to 14 clubs, 4 competition teams and counting. These
-            clubs' interests are spread over a wide spectrum primarily fostering
-            innovation and invention in technology, with a focus on technologies
-            aimed at contributing positively to the environment and to the
-            society. Several CFI projects have been patented and have received
-            national recognition. The team frequently represents IIT Madras in
-            national and international competitions. It also constitutes an
-            integral part of IIT Madras' entrepreneurial culture, with many CFI
-            projects turning into startups.
-          </Typography>
-        </Grid>
-        <Grid item>
           <iframe
             width="600px"
             height="400px"
@@ -53,6 +36,45 @@ function CfiVideo() {
             title="CFI"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
+        </Grid>
+        <Grid item>
+          <ol>
+            <li>
+              <Typography variant="body1" color="primary">
+                The Centre for Innovation (CFI) was established on the premise
+                that innovation emerges through community interaction and the
+                freedom to learn and experiment. Constituted in 2008, with the
+                help of funds raised by the class of 1981, this unique 24/7
+                student lab is located at the New Academic Complex in the heart
+                of campus.
+              </Typography>
+            </li>
+            <li>
+              <Typography variant="body1" color="primary">
+                After almost a decade of the team's zeal and passion, combined
+                with a comprehensive inventory, ample workspace, guidance from
+                faculty and students, CFI is a house to 14 clubs, 4 competition
+                teams and counting.
+              </Typography>
+            </li>
+            <li>
+              <Typography variant="body1" color="primary">
+                These clubs' interests are spread over a wide spectrum primarily
+                fostering innovation and invention in technology, with a focus
+                on technologies aimed at contributing positively to the
+                environment and to the society.
+              </Typography>
+            </li>
+            <li>
+              <Typography variant="body1" color="primary">
+                Several CFI projects have been patented and have received
+                national recognition. The team frequently represents IIT Madras
+                in national and international competitions. It also constitutes
+                an integral part of IIT Madras' entrepreneurial culture, with
+                many CFI projects turning into startups.
+              </Typography>
+            </li>
+          </ol>
         </Grid>
       </Grid>
     </Grid>
