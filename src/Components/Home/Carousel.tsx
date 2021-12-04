3b37ed5da211e8d55eb3d9a6534767f4e346cd3d
gautamV19 <ee20b144@smail.iitm.ac.in>
2021-12-04 17:03:00 +0530

    cfi clubs made responsive

--- src/Components/Home/Carousel.tsx
+++ src/Components/Home/Carousel.tsx
@@ -3,63 +3,55 @@ import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 import car from "../../Images/Home/Landing images/car.jpg";
 import car2 from "../../Images/Home/Landing images/car2.jpg";
 import dron from "../../Images/Home/Landing images/dron.jpg";
 import rover from "../../Images/Home/Landing images/rover.jpg";
 import Heading from "../../Widgets/Heading";
 
-export default class TopCarousel extends Component {
-  render() {
-    return (
-      <Carousel
-        autoPlay
-        interval={5000}
-        autoFocus
-        dynamicHeight
-        swipeable
-        infiniteLoop
-        showStatus={false}
-        showThumbs={false}
-        stopOnHover={false}
-      >
-        <div style={{ height: "92vh" }}>
-          <img src={car} style={{ height: "inherit" }} />
-          <p className="legend" style={{ backgroundColor: "transparent" }}>
-            <Heading
-              white="India's largest student run "
-              red="innovation lab"
-            />
-          </p>
-        </div>
-        <div style={{ height: "92vh" }}>
-          <img src={car2} style={{ height: "inherit" }} />
-          <p className="legend" style={{ backgroundColor: "transparent" }}>
-            <Heading
-              white="India's largest student run "
-              red="innovation lab"
-            />
-          </p>
-        </div>
-        <div style={{ height: "92vh" }}>
-          <img src={dron} style={{ height: "inherit" }} />
-          <p className="legend" style={{ backgroundColor: "transparent" }}>
-            <Heading
-              white="India's largest student run "
-              red="innovation lab"
-            />
-          </p>
-        </div>
-        <div style={{ height: "92vh" }}>
-          <img src={rover} style={{ height: "inherit" }} />
-          <p className="legend" style={{ backgroundColor: "transparent" }}>
-            <Heading
-              white="India's largest student run "
-              red="innovation lab"
-            />
-          </p>
-        </div>
-      </Carousel>
-    );
-  }
+export default function TopCarousel() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <Carousel
+      autoPlay
+      interval={5000}
+      autoFocus
+      dynamicHeight
+      swipeable
+      infiniteLoop
+      showStatus={false}
+      showThumbs={false}
+      stopOnHover={false}
+    >
+      <div style={{ height: `${matches ? "40vh" : "92vh"}` }}>
+        <img src={car} style={{ height: "inherit" }} />
+        <p className="legend" style={{ backgroundColor: "transparent" }}>
+          <Heading white="India's largest student run " red="innovation lab" />
+        </p>
+      </div>
+      <div style={{ height: `${matches ? "40vh" : "92vh"}` }}>
+        <img src={car2} style={{ height: "inherit" }} />
+        <p className="legend" style={{ backgroundColor: "transparent" }}>
+          <Heading white="India's largest student run " red="innovation lab" />
+        </p>
+      </div>
+      <div style={{ height: `${matches ? "40vh" : "92vh"}` }}>
+        <img src={dron} style={{ height: "inherit" }} />
+        <p className="legend" style={{ backgroundColor: "transparent" }}>
+          <Heading white="India's largest student run " red="innovation lab" />
+        </p>
+      </div>
+      <div style={{ height: `${matches ? "40vh" : "92vh"}` }}>
+        <img src={rover} style={{ height: "inherit" }} />
+        <p className="legend" style={{ backgroundColor: "transparent" }}>
+          <Heading white="India's largest student run " red="innovation lab" />
+        </p>
+      </div>
+    </Carousel>
+  );
 }
