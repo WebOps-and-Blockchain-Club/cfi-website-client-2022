0bf3351c0207e7a63cf4200be2177ac79ed3bbef
gautamV19 <ee20b144@smail.iitm.ac.in>
2021-12-06 11:36:27 +0530

    basic responsive navbar

--- src/Components/Route.tsx
+++ src/Components/Route.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Achievements from "./Achievements";
+import Clubs from "./Clubs";
 import Home from "./Home";
+import CompationTeams from "../Components/CompetitionTeams";
 import Navbar from "./Shared/Navbar";
 
 interface Probs {}
@@ -11,6 +14,9 @@ const Routes = (probs: Probs) => {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/clubs" component={Clubs} />
+        <Route exact path="/achievements" component={Achievements} />
+        <Route exact path="/competitionteams" component={CompationTeams} />
       </Switch>
     </BrowserRouter>
   );
--- src/Components/Shared/Navbar.tsx
+++ src/Components/Shared/Navbar.tsx
@@ -1,98 +1,61 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
+import React from "react";
+import {
+  Grid,
+  ButtonGroup,
+  Button,
+  Box,
+  Typography,
+  Avatar,
+} from "@mui/material";
+import myPhoto from "../assets/Images/myPhoto.png";
+import { Link } from "react-router-dom";
 
-interface StyledTabsProps {
-  children?: React.ReactNode;
-  value: number;
-  onChange: (event: React.SyntheticEvent, newValue: number) => void;
-}
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-const StyledTabs = styled((props: StyledTabsProps) => (
-  <Tabs
-    {...props}
-    variant="fullWidth"
-    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
-  />
-))({
-  "& .MuiTabs-indicator": {
-    display: "flex",
-    justifyContent: "center",
-    backgroundColor: "transparent",
-  },
-  "& .MuiTabs-indicatorSpan": {
-    maxWidth: 40,
-    width: "100%",
-    backgroundColor: "red",
-  },
-});
-
-interface StyledTabProps {
-  label: string;
-}
-
-const StyledTab = styled((props: StyledTabProps) => (
-  <Tab disableRipple {...props} />
-))(({ theme }) => ({
-  textTransform: "none",
-  background: "inherit",
-  fontWeight: theme.typography.fontWeightRegular,
-  fontSize: theme.typography.pxToRem(20),
-  marginRight: theme.spacing(1),
-  color: "rgba(255, 255, 255, 0.7)",
-  "&.Mui-selected": {
-    color: "#fff",
-    fontWeight: 700,
-  },
-  "&.Mui-focusVisible": {
-    backgroundColor: "rgba(100, 95, 228, 0.32)",
-  },
-}));
-
-export default function CustomizedTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
+function NavBar() {
   return (
-    <Box sx={{ background: "linear-gradient(black 50%, rgb(56, 56, 56))" }}>
-      <StyledTabs
-        value={value}
-        onChange={handleChange}
-        aria-label="styled tabs example"
-      >
-        <StyledTab label="Home" />
-        <StyledTab label="Clubs" />
-        <StyledTab label="Achivements" />
-        <StyledTab label="Compitative Teams" />
-        <StyledTab label="Alumni" />
-        <StyledTab label="About us" />
-      </StyledTabs>
-      <Box sx={{ p: 1.3 }} />
-    </Box>
+    <ButtonGroup
+      size="large"
+      variant="text"
+      aria-label="text button group"
+      style={{ color: "black" }}
+    >
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <Button style={{ color: "black" }}>Home</Button>
+      </Link>
+      <Link to="/clubs" style={{ textDecoration: "none" }}>
+        <Button style={{ color: "black" }}>Clubs</Button>
+      </Link>
+      <Link to="/achievements" style={{ textDecoration: "none" }}>
+        <Button style={{ color: "black" }}>Achievements</Button>
+      </Link>
+      <Link to="/competitionteams" style={{ textDecoration: "none" }}>
+        <Button style={{ color: "black" }}>Compation Teams</Button>
+      </Link>
+      <Link to="/achievements" style={{ textDecoration: "none" }}>
+        <Button style={{ color: "black" }}>Media</Button>
+      </Link>
+      <Link to="/achievements" style={{ textDecoration: "none" }}>
+        <Button style={{ color: "black" }}>Alumni</Button>
+      </Link>
+      <Link to="/achievements" style={{ textDecoration: "none" }}>
+        <Button style={{ color: "black" }}>About Us</Button>
+      </Link>
+      <Link to="/achievements" style={{ textDecoration: "none" }}>
+        <Button style={{ color: "black" }}>Contact Us</Button>
+      </Link>
+    </ButtonGroup>
   );
 }
 
-/*
+function NavMenu() {
+  return <div>NavMenu</div>;
+}
 
-<Box sx={{ bgcolor: "#000" }}>
-    <StyledTabs
-    value={value}
-    onChange={handleChange}
-    aria-label="styled tabs example"
-    >
-    <StyledTab label="Home" />
-    <StyledTab label="Clubs" />
-    <StyledTab label="Achivements" />
-    <StyledTab label="Compitative Teams" />
-    <StyledTab label="Alumni" />
-    <StyledTab label="About us" />
-    </StyledTabs>
-    <Box sx={{ p: 3 }} />
-</Box>
+export default function Header() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
 
-*/
+  return matches ? <NavMenu /> : <NavBar />;
+}
