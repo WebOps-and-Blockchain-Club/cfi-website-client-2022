02c6eecb1e4baf15f21b57b3c6c59315c2f0e55d
JanithMS <janithms9920@gmail.com>
2022-01-28 20:06:53 +0530

    reversed color scheme for favicon

--- public/script.js
+++ public/script.js
@@ -1,28 +1,31 @@
-'use strict';
+"use strict";
 
 function setupIcons() {
-  const lightSchemeIcon = document.querySelector('link#light-scheme-icon');
-  const darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+  const lightSchemeIcon = document.querySelector("link#light-scheme-icon");
+  const darkSchemeIcon = document.querySelector("link#dark-scheme-icon");
 
-  const lightSchemeManifest = document.querySelector('link#light-scheme-manifest');
-  const darkSchemeManifest = document.querySelector('link#dark-scheme-manifest');
-  
-  function setLight() {
+  const lightSchemeManifest = document.querySelector(
+    "link#light-scheme-manifest"
+  );
+  const darkSchemeManifest = document.querySelector(
+    "link#dark-scheme-manifest"
+  );
+
+  function setDark() {
     document.head.append(lightSchemeIcon);
     document.head.append(lightSchemeManifest);
     darkSchemeIcon.remove();
     darkSchemeManifest.remove();
   }
 
-  function setDark() {
+  function setLight() {
     lightSchemeIcon.remove();
     lightSchemeManifest.remove();
     document.head.append(darkSchemeIcon);
     document.head.append(darkSchemeManifest);
   }
 
-
-  const matcher = window.matchMedia('(prefers-color-scheme:dark)');
+  const matcher = window.matchMedia("(prefers-color-scheme:dark)");
   function onUpdate() {
     if (matcher.matches) {
       setDark();
